0ad228940696d4f1134e2401ad8d60393bb92122
Chris <c.daly888@gmail.com>
2024-03-28 12:50:35 +0000

    Refactored 5.rebuild_initram.sh

---
+++ scripts/command/5.rebuild_initram.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+rasp_boot_dir="/boot/firmware"
+
+mkinitramfs -o ${rasp_boot_dir}/initramfs.gz
+lsinitramfs ${rasp_boot_dir}/initramfs.gz |grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
+#sudo reboot
