14622e2918402d1471f113e91f9d65168dfa6d07
Chris <c.daly888@gmail.com>
2024-03-29 14:07:07 +0000

    Added reboot option to  1.update.sh

--- scripts/command/1.update.sh
+++ scripts/command/1.update.sh
@@ -3,5 +3,13 @@
 # update OS files
 sudo apt update && sudo apt upgrade -y
 #sudo rpi-update
-echo "Done. Reboot with: sudo reboot"
+echo "Done. Reboot needed"
 #reboot #needed to load new kernel
+printf "Do you want to reboot now? \n Y or N"
+read -r decision
+
+if $decision '==' Y; then
+    sudo reboot now
+else
+    exit
+fi
