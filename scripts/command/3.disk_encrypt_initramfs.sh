490a3a99da7645374d50f28a8737484ff9416ec6
Chris <c.daly888@gmail.com>
2024-03-29 14:08:55 +0000

    Refactored 3.disk_encrypt_initramfs.sh

--- scripts/command/3.disk_encrypt_initramfs.sh
+++ scripts/command/3.disk_encrypt_initramfs.sh
@@ -8,14 +8,14 @@ resize2fs -fM $root_dev_name_loc
 BLOCK_COUNT="$(dumpe2fs ${root_dev_name_loc} | sed "s/ //g" | sed -n "/Blockcount:/p" | cut -d ":" -f 2)"
 echo "Block count: ${BLOCK_COUNT}"
 SHA1SUM_ROOT="$(dd bs=4k count=${BLOCK_COUNT} if=${root_dev_name_loc} | sha1sum)"
-dd bs=4k count="$BLOCK_COUNT" if=$root_dev_name_loc of=/dev/$1
+dd bs=4k count="$BLOCK_COUNT" if="$root_dev_name_loc" of=/dev/$1
 SHA1SUM_EXT="$(dd bs=4k count=${BLOCK_COUNT} if=/dev/$1 | sha1sum)"
 
 if [ "$SHA1SUM_ROOT" == "$SHA1SUM_EXT" ]; then
 	echo "1.Sha1sums match."
 	cryptsetup --cipher aes-xts:sha256 luksFormat ${root_dev_name_loc}
 	cryptsetup luksOpen ${root_dev_name_loc} sdcard
-	dd bs=4k count="$BLOCK_COUNT" if=/dev/$1 of=/dev/mapper/sdcard
+	dd bs=4k count="$BLOCK_COUNT" if=/dev/"$1" of=/dev/mapper/sdcard
 	SHA1SUM_NEWROOT="$(dd bs=4k count=1516179 if=/dev/mapper/sdcard | sha1sum)"
 	if [ "$SHA1SUM_NEWROOT" == "$SHA1SUM_EXT" ]; then
 		echo "2.Sha1sums match."
