75168fa8c83f6055074537455832884ecfd64f42
Chris <c.daly888@gmail.com>
2024-03-29 20:39:16 +0000

    Update disk encrypt script

--- scripts/command/2.disk_encrypt.sh
+++ scripts/command/2.disk_encrypt.sh
@@ -10,13 +10,11 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-install_command="apt install"
-install_dir="/boot/install"
 rasp_boot_dir="/boot/firmware"
+install_dir="${rasp_boot_dir}/install"
 root_dev_name="mmcblk0p2"
 
-$install_command busybox cryptsetup initramfs-tools -y
-$install_command expect --no-install-recommends -y
+
 cp ${install_dir}/initramfs-rebuild /etc/kernel/postinst.d/initramfs-rebuild
 cp ${install_dir}/resize2fs /etc/initramfs-tools/hooks/resize2fs
 chmod +x /etc/kernel/postinst.d/initramfs-rebuild
@@ -33,21 +31,19 @@ echo 'initramfs initramfs.gz followkernel' | tee --append ${rasp_boot_dir}/confi
 sed -i '$s/$/ cryptdevice=\/dev\/mmcblk0p2:sdcard/' ${rasp_boot_dir}/cmdline.txt
 
 ROOT_CMD="$(sed -n 's|^.*root=\(\S\+\)\s.*|\1|p' ${rasp_boot_dir}/cmdline.txt)"
-sed -i -e "s|$ROOT_CMD|/dev/mapper/sdcard|g" ${rasp_boot_dir}/cmdline.txt
+sed -i -e "s|${ROOT_CMD}|/dev/mapper/sdcard|g" ${rasp_boot_dir}/cmdline.txt
 
 FSTAB_CMD="$(blkid | sed -n '/dev\/mmcblk0p2/s/.*\ PARTUUID=\"\([^\"]*\)\".*/\1/p')"
-sed -i -e "s|PARTUUID=$FSTAB_CMD|/dev/mapper/sdcard|g" /etc/fstab
+sed -i -e "s|PARTUUID=${FSTAB_CMD}|/dev/mapper/sdcard|g" /etc/fstab
 
 echo "sdcard /dev/${root_dev_name} none luks" | tee --append /etc/crypttab > /dev/null
 
 echo "Done. Reboot needed"
 
 printf "Do you want to reboot now? \n Y or N"
-read -r decision
+read decision
 
 if $decision '==' Y; then
    sudo reboot now
-else
-   exit
 fi
 #reboot
