3befdc03c3a6ae54b2b5cf9765433ea74ba65953
Chris <c.daly888@gmail.com>
2024-04-02 13:55:36 +0100

    Updated disk_encrypt script

--- scripts/command/2.disk_encrypt.sh
+++ scripts/command/2.disk_encrypt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #https://github.com/johnshearing/MyEtherWalletOffline/blob/master/Air-Gap_Setup.md#setup-luks-full-disk-encryption
 #https://robpol86.com/raspberry_pi_luks.html
@@ -10,40 +10,34 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-rasp_boot_dir="/boot/firmware"
-install_dir="${rasp_boot_dir}/install"
-root_dev_name="mmcblk0p2"
-
-
-cp ${install_dir}/initramfs-rebuild /etc/kernel/postinst.d/initramfs-rebuild
-cp ${install_dir}/resize2fs /etc/initramfs-tools/hooks/resize2fs
+cp /boot/firmware/install/initramfs-rebuild /etc/kernel/postinst.d/initramfs-rebuild
+cp /boot/firmware/install/resize2fs /etc/initramfs-tools/hooks/resize2fs
 chmod +x /etc/kernel/postinst.d/initramfs-rebuild
 chmod +x /etc/initramfs-tools/hooks/resize2fs
 
-echo 'CRYPTSETUP=y' | tee --append /etc/cryptsetup-initramfs/conf-hook > /dev/null
-mkinitramfs -o ${rasp_boot_dir}/initramfs.gz
+mkdir -p /etc/cryptsetup-initramfs/
 
-lsinitramfs ${rasp_boot_dir}/initramfs.gz | grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
+filename=/etc/cryptsetup-initramfs/conf-hook
+test -f $filename || touch $filename
+
+echo 'CRYPTSETUP=y' | tee --append /etc/cryptsetup-initramfs/conf-hook > /dev/null
+mkinitramfs -o /boot/firmware/initramfs.gz
+
+lsinitramfs /boot/firmware/initramfs.gz | grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
 #Make sure you see sbin/resize2fs, sbin/cryptsetup, and sbin/fdisk in the output.
 
-echo 'initramfs initramfs.gz followkernel' | tee --append ${rasp_boot_dir}/config.txt > /dev/null
+echo 'initramfs initramfs.gz followkernel' | tee --append /boot/firmware/config.txt > /dev/null
 
-sed -i '$s/$/ cryptdevice=\/dev\/mmcblk0p2:sdcard/' ${rasp_boot_dir}/cmdline.txt
+sed -i '$s/$/ cryptdevice=\/dev\/mmcblk0p2:sdcard/' /boot/firmware/cmdline.txt
 
-ROOT_CMD="$(sed -n 's|^.*root=\(\S\+\)\s.*|\1|p' ${rasp_boot_dir}/cmdline.txt)"
-sed -i -e "s|${ROOT_CMD}|/dev/mapper/sdcard|g" ${rasp_boot_dir}/cmdline.txt
+ROOT_CMD="$(sed -n 's|^.*root=\(\S\+\)\s.*|\1|p' /boot/firmware/cmdline.txt)"
+sed -i -e "s|${ROOT_CMD}|/dev/mapper/sdcard|g" /boot/firmware/cmdline.txt
 
 FSTAB_CMD="$(blkid | sed -n '/dev\/mmcblk0p2/s/.*\ PARTUUID=\"\([^\"]*\)\".*/\1/p')"
 sed -i -e "s|PARTUUID=${FSTAB_CMD}|/dev/mapper/sdcard|g" /etc/fstab
 
-echo "sdcard /dev/${root_dev_name} none luks" | tee --append /etc/crypttab > /dev/null
+echo "sdcard /dev/mmcblk0p2 none luks" | tee --append /etc/crypttab > /dev/null
 
 echo "Done. Reboot needed"
 
-printf "Do you want to reboot now? \n Y or N"
-read decision
-
-if $decision '==' Y; then
-   sudo reboot now
-fi
 #reboot
