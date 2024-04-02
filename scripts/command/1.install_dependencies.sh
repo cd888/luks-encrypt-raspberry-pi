d50846a6d7e20c862eb68ccb71ffb1e61ece65d6
Chris <c.daly888@gmail.com>
2024-04-02 13:54:38 +0100

    Updated 1.install_dependencies

--- scripts/command/1.install_dependencies.sh
+++ scripts/command/1.install_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # update OS files
 sudo apt update && sudo apt upgrade -y
@@ -11,10 +11,3 @@ $install_command expect --no-install-recommends -y
 
 #sudo rpi-update
 echo "Done. Reboot needed"
-
-printf "Do you want to reboot now? \n Y or N"
-read decision
-
-if $decision '==' Y; then
-    sudo reboot now
-fi
